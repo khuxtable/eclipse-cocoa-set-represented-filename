853e5032abbd12caf825dd752b01c7782fd1406b
Kathryn Huxtable <kathryn@kathrynhuxtable.org>
2010-06-25 15:06:08 -0500

    Moved SetMacDocumentHandler to main package. Added some comments. Added package restriction for Mac OS X Cocoa on Intel. Factored actual code to set the represented filename into new class CocoaRepresentedFilename.

---
+++ src/org/kathrynhuxtable/eclipse/plugin/setmacdocument/CocoaRepresentedFilename.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2010 Kathryn Huxtable
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at
+ *
+ *   http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kathrynhuxtable.eclipse.plugin.setmacdocument;
+
+import org.eclipse.core.resources.IFile;
+
+import org.eclipse.swt.internal.cocoa.NSString;
+import org.eclipse.swt.internal.cocoa.NSView;
+import org.eclipse.swt.internal.cocoa.NSWindow;
+import org.eclipse.swt.internal.cocoa.OS;
+import org.eclipse.swt.widgets.Shell;
+
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPartSite;
+
+/**
+ * Set the represented filename associated with the active editor, or remove it
+ * if no file is associated, or there is no active editor.
+ * 
+ * @author Kathryn Huxtable
+ */
+@SuppressWarnings("restriction")
+public class CocoaRepresentedFilename {
+
+    /**
+     * Set the represented filename for the Cocoa NSWindow associated with the
+     * active editor to the filename in that editor, or set it to empty string
+     * if no file is associated.
+     *
+     * @param site the workbench part's site.
+     */
+    public void setRepresentedFilename(IWorkbenchPartSite site) {
+        Shell shell = site.getShell();
+
+        if (shell != null) {
+            NSView view = shell.view;
+
+            if (view != null) {
+                NSWindow w = view.window();
+
+                if (w != null) {
+                    // Create the Objective C selector for the method to set the filename,
+                    // and get the filename.
+                    long     sel     = OS.sel_registerName("setRepresentedFilename:");
+                    NSString fileStr = getFilename(site);
+
+                    // Set the represented filename.
+                    OS.objc_msgSend(w.id, sel, fileStr.id);
+                }
+            }
+        }
+    }
+
+    /**
+     * Retrieve the filename associated with the current workbench part.
+     *
+     * @param  site the workbench part's site.
+     *
+     * @return an NSString representing the filename, or an empty NSString if no
+     *         filename is associated.
+     */
+    private NSString getFilename(IWorkbenchPartSite site) {
+        String filename = "";
+
+        if (site != null && site.getPage() != null) {
+            IEditorPart editor = site.getPage().getActiveEditor();
+
+            if (editor != null && editor.getEditorInput() != null) {
+                IFile file = (IFile) editor.getEditorInput().getAdapter(IFile.class);
+
+                if (file != null) {
+                    filename = file.getLocation().toOSString();
+                }
+            }
+        }
+
+        return org.eclipse.swt.internal.cocoa.NSString.stringWith(filename);
+    }
+}
---
+++ src/org/kathrynhuxtable/eclipse/plugin/setmacdocument/SetMacDocumentHandler.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2010 Kathryn Huxtable
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at
+ *
+ *   http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kathrynhuxtable.eclipse.plugin.setmacdocument;
+
+import org.eclipse.core.expressions.EvaluationResult;
+import org.eclipse.core.expressions.Expression;
+import org.eclipse.core.expressions.ExpressionInfo;
+import org.eclipse.core.expressions.IEvaluationContext;
+import org.eclipse.core.runtime.CoreException;
+
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
+
+import org.eclipse.ui.ISources;
+import org.eclipse.ui.IStartup;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.services.IEvaluationService;
+
+/**
+ * Startup class.
+ *
+ * <p>This runs at startup and registers a listener on changes to active
+ * editor.</p>
+ * 
+ * @author Kathryn Huxtable
+ */
+public class SetMacDocumentHandler implements IStartup {
+
+    /**
+     * @see org.eclipse.ui.IStartup#earlyStartup()
+     */
+    @Override
+    public void earlyStartup() {
+        // Get the evaluation service.
+        IEvaluationService service = (IEvaluationService) PlatformUI.getWorkbench().getService(IEvaluationService.class);
+
+        // Register a listener on active editor name property changes.
+        service.addEvaluationListener(new ActiveEditorChangeExpression(),
+                                      new ActiveEditorPropertyChangeListener(service),
+                                      ISources.ACTIVE_EDITOR_NAME);
+    }
+
+    /**
+     * Detect changes in active editor name.
+     */
+    private static final class ActiveEditorChangeExpression extends Expression {
+
+        private boolean rc = false;
+
+        /**
+         * @see org.eclipse.core.expressions.Expression#collectExpressionInfo(org.eclipse.core.expressions.ExpressionInfo)
+         */
+        @Override
+        public void collectExpressionInfo(ExpressionInfo info) {
+            info.addVariableNameAccess(ISources.ACTIVE_EDITOR_NAME);
+        }
+
+        /**
+         * @see org.eclipse.core.expressions.Expression#evaluate(org.eclipse.core.expressions.IEvaluationContext)
+         */
+        @Override
+        public EvaluationResult evaluate(IEvaluationContext context) throws CoreException {
+            rc = !rc;
+            return EvaluationResult.valueOf(rc);
+        }
+    }
+
+    /**
+     * Property change listener for changes to active editor name.
+     */
+    private static final class ActiveEditorPropertyChangeListener implements IPropertyChangeListener {
+
+        private IEvaluationService       service;
+        private CocoaRepresentedFilename representedFilename;
+
+        /**
+         * Creates a new ActiveEditorPropertyChangeListener object.
+         *
+         * @param service the evaluation service we're registered with.
+         */
+        ActiveEditorPropertyChangeListener(IEvaluationService service) {
+            this.service             = service;
+            this.representedFilename = new CocoaRepresentedFilename();
+        }
+
+        /**
+         * @see org.eclipse.jface.util.IPropertyChangeListener#propertyChange(org.eclipse.jface.util.PropertyChangeEvent)
+         */
+        @Override
+        public void propertyChange(PropertyChangeEvent event) {
+            Object part = service.getCurrentState().getVariable(ISources.ACTIVE_PART_NAME);
+
+            // If the active part is an IWorkbenchPart, get its site and set the filename, if any.
+            if (part != null && part instanceof IWorkbenchPart) {
+                representedFilename.setRepresentedFilename(((IWorkbenchPart) part).getSite());
+            }
+        }
+    }
+}
